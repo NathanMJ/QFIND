d0239282abce7d819e912631f38eaa0bb91b7238
NathanMJ <nathanmimoun2001@gmail.com>
2026-03-27 16:16:18 +0300

    security

---
+++ app.config.js
@@ -0,0 +1,22 @@
+export default ({ config }) => {
+  return {
+    ...config,
+    ios: {
+      ...config.ios,
+      config: {
+        ...config.ios?.config,
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      }
+    },
+    android: {
+      ...config.android,
+      config: {
+        ...config.android?.config,
+        googleMaps: {
+          ...config.android?.config?.googleMaps,
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      }
+    }
+  };
+};
