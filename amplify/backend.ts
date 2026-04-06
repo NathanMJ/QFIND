9447e0f1b404b25a2953fc6663ea91e3d3cf6076
QFIND Project <cto@qfind.io>
2026-04-06 15:35:37 +0300

    Update backend.ts
    Integration the backend about S3 bucket and Lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { presignedUrlFunction } from './functions/presigned-url/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -10,4 +11,12 @@ defineBackend({
   auth,
   data,
   storage,
+  presignedUrlFunction
 });
+
+// Grant the Lambda read access to the S3 bucket
+const s3Bucket = backend.storage.resources.bucket;
+const lambdaFn = backend.PresignedUrlFunction.resources.lambda;
+
+s3Bucket.grantRead(lambdaFn);
+lambdaFn.addEnvironment("S3_BUCKET_NAME", s3Bucket.bucketName);
